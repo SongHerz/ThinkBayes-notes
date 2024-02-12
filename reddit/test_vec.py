0302d3aa70b7825625638c4f6b136f455a001797
hongzhi <dldz2002@outlook.com>
2024-02-12 18:42:51 +0800

    More work on reddit test vec

--- reddit/test_vec.py
+++ reddit/test_vec.py
@@ -3,7 +3,9 @@
 Generate test vector
 """
 
-import random
+import math
+from random import Random
+import numpy as np
 
 from .comm import VoteDir
 
@@ -13,19 +15,73 @@ def gen_test_vec() -> list[tuple[int, int, VoteDir]]:
     Generate test vector.
     :return: list of test vectors in [(user id, link id, vote dir)]
     """
-    user_ids = list(range(10))
-    link_ids = list(range(15))
+    rand = Random(100)
 
-    ret = []
+    USER_COUNT = 5
+    LINK_COUNT = 15
+    user_ids = list(range(USER_COUNT))
+
+    GOOD_LINK_RATIO = 0.8
+
+    # Classify predefined good / bad links
+    last_good_link_idx_plus1 = math.floor(LINK_COUNT * GOOD_LINK_RATIO)
+    good_link_ids = list(range(0, last_good_link_idx_plus1))
+    bad_link_ids = list(range(last_good_link_idx_plus1, LINK_COUNT))
+
+    # Assign user reliability from [1.0 to 0.0]
+    uid_reli_map = {}
+    uid_reli_map.update(x for x in zip(user_ids, list(np.linspace(start=1.0, stop=0.0, num=len(user_ids), dtype=float))))
+
+    # {(uid, link id): vote_dir}
+    vote_map = {}
     for uid in user_ids:
-        for _ in link_ids:
-            link_id = random.choice(link_ids)
-            dir_ = random.randint(0, 1)
+        for link_id in good_link_ids:
+            # link_id = rand.choice(link_ids)
+            # dir_ = rand.randint(0, 1)
+            # FIXME: calculate dir based on good/bad link and user reliability
+            dir_ = 0
+
+            k = (uid, link_id)
+            if k in vote_map:
+                # Only the first random vote sound.
+                continue
+
             if dir_ == 0:
                 vote_dir = VoteDir.UP
             else:
                 vote_dir = VoteDir.DOWN
 
-            ret.append((uid, link_id, vote_dir))
+            vote_map[k] = vote_dir
 
-    return ret
+        for link_id in bad_link_ids:
+            # FIXME: calculate dir based on good/bad link and user reliability
+            dir_ = 1
+
+            k = (uid, link_id)
+            if k in vote_map:
+                # Only the first random vote sound.
+                continue
+
+            if dir_ == 0:
+                vote_dir = VoteDir.UP
+            else:
+                vote_dir = VoteDir.DOWN
+
+            vote_map[k] = vote_dir
+
+    print()
+    print('#####################')
+    print(' Test Vector Summary')
+    print('#####################')
+    print()
+    print('# Link Quality')
+    print(f'Good Links: {good_link_ids}')
+    print(f'Bad Links: {bad_link_ids}')
+    print()
+    print('# User reliability')
+    for uid, reli in uid_reli_map.items():
+        print(f'user: {uid}, reliability: {reli}')
+
+    print()
+
+    return [(user_id, link_id, vote_dir) for (user_id, link_id), vote_dir in vote_map.items()]
